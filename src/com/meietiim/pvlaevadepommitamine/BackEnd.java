8fe343fc78d0dd347cc767d3459cb3b98b96951a
Matrx007 <raintz1000@gmail.com>
2020-01-24 18:56:22 +0200

    Added out-of-bounds check Added comments

--- src/com/meietiim/pvlaevadepommitamine/BackEnd.java
+++ src/com/meietiim/pvlaevadepommitamine/BackEnd.java
@@ -3,29 +3,46 @@ package com.meietiim.pvlaevadepommitamine;
 import static com.meietiim.pvlaevadepommitamine.FrontEnd.*;
 
 public class BackEnd {
-    int playerPlaceShipX = FrontEnd.MAIN.playerPlaceShipX;
-    int playerPlaceShipY = FrontEnd.MAIN.playerPlaceShipY;
-    int playerPlaceShipH = FrontEnd.MAIN.playerPlaceShipH;
-    int playerPlaceShipW = FrontEnd.MAIN.playerPlaceShipW;
+    int playerPlaceShipX = MAIN.playerPlaceShipX;
+    int playerPlaceShipY = MAIN.playerPlaceShipY;
+    int playerPlaceShipH = MAIN.playerPlaceShipH;
+    int playerPlaceShipW = MAIN.playerPlaceShipW;
+    
     public void update() {
-        switch (FrontEnd.MAIN.action) {
-            case ACTION_PLACE_SHIP; // Player placed a ship
+        switch (MAIN.action) {
+            case ACTION_PLACE_SHIP: // Player placed a ship
                 // Store placed ship in playerShip
-                for(int x = playerPlaceShipX; x < playerPlaceShipX+playerPlaceShipW -1; x++) {
-                    for(int y = playerPlaceShipY; y < playerPlaceShipY+playerPlaceShipH -1; y++) {
-                        if(x > 0 && y > 0 && x < 10 && y < 10) playerShips[x][y] = true;
-                        else FrontEnd.MAIN.ERROR_UNKNOWN = 2;
+                
+                // Check if ship inside bounds
+                boolean shipInsideBounds = true;
+                if(playerPlaceShipX > 0 && playerPlaceShipY > 0 &&
+                        playerPlaceShipX+playerPlaceShipW < 10 &&
+                        playerPlaceShipY+playerPlaceShipH < 10) {
+                    shipInsideBounds = false;
+                }
+                
+                // Ship is inside the bounds, proceed to process the ship
+                if(shipInsideBounds) {
+                    for (int x = playerPlaceShipX; x < playerPlaceShipX + playerPlaceShipW - 1; x++) {
+                        for (int y = playerPlaceShipY; y < playerPlaceShipY + playerPlaceShipH - 1; y++) {
+                            if (x > 0 && y > 0 && x < 10 && y < 10)
+                                playerShips[x][y] = true;
+                            // TODO: IDK
+                            else MAIN.error = ERROR_UNKNOWN;
+                        }
                     }
+                } else {
+                    MAIN.error = ERROR_INCORRECT_PLACEMENT;
                 }
                 break;
-            case ACTION_PLACE_BOMB; // Player placed a bomb
+            case ACTION_PLACE_BOMB: // Player placed a bomb
                 if ()
                     break;
-            case ACTION_PLACE_COMPUTER; // Computer's turn
+            case ACTION_PLACE_COMPUTER: // Computer's turn
 
                 break;
             default:
-                FrontEnd.MAIN.error = ERROR_UNKNOWN;
+                MAIN.error = ERROR_UNKNOWN;
         }
     }
 }
